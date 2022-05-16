95351896a9822a370c62a7134bb86e186bc5a823
xjixzz <101193127+xjixzz@users.noreply.github.com>
2022-05-16 20:29:43 +0800

    Add files via upload

---
+++ datasets/seasondepth_mono_dataset.py
@@ -0,0 +1,186 @@
+# Copyright Niantic 2019. Patent Pending. All rights reserved.
+#
+# This software is licensed under the terms of the Monodepth2 licence
+# which allows for non-commercial use only, the full terms of which are made
+# available in the LICENSE file.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import random
+import numpy as np
+import copy
+from PIL import Image  # using pillow-simd for increased speed
+
+import torch
+import torch.utils.data as data
+from torchvision import transforms
+from torchvision.transforms import InterpolationMode
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning
+    # (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+
+
+class MonoDataset(data.Dataset):
+    """Superclass for monocular dataloaders
+
+    Args:
+        data_path
+        filenames
+        height
+        width
+        frame_idxs
+        scales
+        is_train
+        img_ext
+    """
+    def __init__(self,
+                 data_path,
+                 filenames,
+                 height,
+                 width,
+                 frame_idxs,
+                 scales,
+                 is_train=False,
+                 img_ext='.jpg'):
+        super(MonoDataset, self).__init__()
+
+        self.data_path = data_path
+        self.filenames = filenames
+        self.height = height
+        self.width = width
+        self.scales = scales
+        #self.interp = Image.ANTIALIAS
+        self.interp = InterpolationMode.LANCZOS #torchvision>0.8.2
+        
+        self.frame_idxs = frame_idxs
+
+        self.is_train = is_train
+        self.img_ext = img_ext
+
+        self.loader = pil_loader
+        self.to_tensor = transforms.ToTensor()
+
+        # We need to specify augmentations differently in newer versions of torchvision.
+        # We first try the newer tuple version; if this fails we fall back to scalars
+        try:
+            self.brightness = (0.8, 1.2)
+            self.contrast = (0.8, 1.2)
+            self.saturation = (0.8, 1.2)
+            self.hue = (-0.1, 0.1)
+            transforms.ColorJitter.get_params(
+                self.brightness, self.contrast, self.saturation, self.hue)
+        except TypeError:
+            self.brightness = 0.2
+            self.contrast = 0.2
+            self.saturation = 0.2
+            self.hue = 0.1
+        
+        self.resize = {}
+        for i in scales[1:]:
+            s = 2 ** i
+            self.resize[i] = transforms.Resize((self.height // s, self.width // s),
+                                               interpolation=self.interp)
+
+    def preprocess(self, inputs, color_aug):
+        """Resize colour images to the required scales and augment if required
+
+        We create the color_aug object in advance and apply the same augmentation to all
+        images in this item. This ensures that all images input to the pose network receive the
+        same augmentation.
+        """
+        for k in list(inputs):
+            f = inputs[k]
+            if "color" in k:
+                n, im, i = k
+                if self.is_train and im == 0:
+                    for i in range(1, len(self.scales)):
+                        inputs[(n, im, self.scales[i])] = self.resize[self.scales[i]](inputs[(n, im, self.scales[i-1])])
+                    for i in self.scales[1:]:
+                        inputs[(n, im, i)] = self.to_tensor(inputs[(n, im, i)])
+                n, im, i = k
+                inputs[(n, im, i)] = self.to_tensor(f)
+                if self.is_train:
+                    inputs[(n + "_aug", im, i)] = self.to_tensor(color_aug(f))
+                        
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, index):
+        """Returns a single training item from the dataset as a dictionary.
+
+        Values correspond to torch tensors.
+        Keys in the dictionary are either strings or tuples:
+
+            ("color", <frame_id>, <scale>)          for raw colour images,
+            ("color_aug", <frame_id>, <scale>)      for augmented colour images,
+            ("K", scale) or ("inv_K", scale)        for camera intrinsics,
+            "stereo_T"                              for camera extrinsics, and
+            "depth_gt"                              for ground truth depth maps.
+
+        <frame_id> is either:
+            an integer (e.g. 0, -1, or 1) representing the temporal step relative to 'index',
+        or
+            "s" for the opposite image in the stereo pair.
+
+        <scale> is an integer representing the scale of the image relative to the fullsize image:
+            -1      images at native resolution as loaded from disk
+            0       images resized to (self.width,      self.height     )
+            1       images resized to (self.width // 2, self.height // 2)
+            2       images resized to (self.width // 4, self.height // 4)
+            3       images resized to (self.width // 8, self.height // 8)
+        """
+        inputs = {}
+
+        do_color_aug = self.is_train and random.random() > 0.5
+        do_flip = self.is_train and random.random() > 0.5
+        
+        if do_color_aug:
+            color_aug = transforms.ColorJitter(
+                self.brightness, self.contrast, self.saturation, self.hue)
+        else:
+            color_aug = (lambda x: x)
+            
+        fs = self.filenames[index].split()
+        
+        indexs = {0:1, -1:2, 1:3}
+        
+        for i in self.frame_idxs:
+                inputs[("color", i, 0)] = self.get_color(fs[0], fs[indexs[i]], do_flip)
+
+        # adjusting intrinsics to match each scale in the pyramid
+        #for scale in range(self.num_scales):
+        if self.is_train:
+            scale = 0
+            K = self.K.copy()
+
+            K[0, :] *= self.width // (2 ** scale)
+            K[1, :] *= self.height // (2 ** scale)
+
+            inv_K = np.linalg.pinv(K)
+
+            inputs[("K", scale)] = torch.from_numpy(K)
+            inputs[("inv_K", scale)] = torch.from_numpy(inv_K)
+
+        self.preprocess(inputs, color_aug)
+
+
+        if (not self.is_train) and self.load_depth:
+            depth_gt = self.get_depth(fs[0], fs[1])
+            inputs["depth_gt"] = np.expand_dims(depth_gt, 0)
+            inputs["depth_gt"] = torch.from_numpy(inputs["depth_gt"].astype(np.float32))
+
+        return inputs
+
+    def get_color(self, folder, frame_index, side, do_flip):
+        raise NotImplementedError
+
+
+    def get_depth(self, folder, frame_index, side, do_flip):
+        raise NotImplementedError
